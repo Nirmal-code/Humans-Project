1cbb20b388ddfcb26aa9353cd263b402fa8bd5cd
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-09-26 01:48:14 -0400

    updated nominate-page and contact-us page to hold image propery

--- humans-proj/src/pages/nominate-page/nominate.js
+++ humans-proj/src/pages/nominate-page/nominate.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './nominate.css';
 import SubmissionForm from '../components/submission-form/submission-form';
+import photo from '../../../static/images/nominate-side-photo.jpg';
 
-function Nominate() {
+export default function Nominate() {
     return (
         <div>
             <h1>Temporary Header</h1>
             <SubmissionForm
                 intro="Would you like to recommend yourself or someone you know to be featured on our website? Enter your name and information below, as well as a short description of the person or group you are recommending."
-                image="../../../static/images/nominate-side-photo.jpg"
+                image={photo}
             />
             <footer style={{ color: "white" }}>Temporary footer</footer>
         </div>
     )
 }
-
-export default Nominate;
