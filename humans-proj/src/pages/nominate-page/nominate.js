9fdbabe5ba2233964be1e96f2726b04a5e145a59
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-10-10 19:00:09 -0400

    added Header to nominate page

--- humans-proj/src/pages/nominate-page/nominate.js
+++ humans-proj/src/pages/nominate-page/nominate.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './nominate.css';
 import SubmissionForm from '../components/submission-form/submission-form';
 import photo from '../../../static/images/nominate-side-photo.jpg';
+import Header from '../components/Header/Header';
 
 export default function Nominate() {
     return (
         <div id="nominate-page">
-            <h1>Temporary Header</h1>
+            <Header />
             <SubmissionForm
                 intro="Would you like to recommend yourself or someone you know to be featured on our website? Enter your name and information below, as well as a short description of the person or group you are recommending."
                 image={photo}
