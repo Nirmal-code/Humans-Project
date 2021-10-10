35ac316739a7aa9405dce6a13b84ed3a8343d1ce
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-10-10 18:20:07 -0400

    replaced temp header with actual header

--- humans-proj/src/pages/contact-us/contact-us.js
+++ humans-proj/src/pages/contact-us/contact-us.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './contact-us.css';
 import SubmissionForm from '../components/submission-form/submission-form';
 import photo from '../../../static/images/contact-us-side-photo.png';
+import Header from '../components/Header/Header';
 
 export default function Contact_Us() {
     return (
         <div id="contact-us-page">
-            <h1>Temporary Header</h1>
+            <Header />
             <SubmissionForm
                 intro="Do you have a message, question, or concern? Contact us with the form below and our team will get in touch with you as soon as possible!"
                 image={photo}
