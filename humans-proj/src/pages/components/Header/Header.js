d8e6313532214dc55b38f0555be29356487934d5
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-10-12 22:54:58 -0400

    added white font and active blue underline for header links

--- humans-proj/src/pages/components/Header/Header.js
+++ humans-proj/src/pages/components/Header/Header.js
@@ -4,6 +4,11 @@ import handPhoto from '../../../../static/images/SFL-humans-logo.png';
 import './Header.css'
 
 export default function Header() {
+    const activeLink = {
+        borderBottom:"2px solid #63ABFF",
+        paddingBottom:"1vh"
+    }
+    
     return (
         <div id="header">
             <img 
@@ -16,20 +21,20 @@ export default function Header() {
             <h1 style={{float: "left"}}>HUMANS</h1>
 
             <div id="links-bar">
-                <li id="header-link">
-                    <Link to="/home/Home/">HOME</Link>
+                <li id="header-list" style={{display:"inline"}}>
+                    <Link to="/home/Home/" id="header-link" style={{marginLeft:"14vw"}} activeStyle={activeLink}>HOME</Link>
                 </li>
                 <li id="header-link">
-                    <Link to="/feature-page/feature-page/">FEATURE</Link>
+                    <Link to="/feature-page/feature-page/" id="header-link" activeStyle={activeLink}>FEATURE</Link>
                 </li>
                 <li id="header-link">
-                    <Link to="/about-page/AboutPage/">ABOUT</Link>
+                    <Link to="/about-page/AboutPage/" id="header-link" activeStyle={activeLink}>ABOUT</Link>
                 </li> 
                 <li id="header-link">
-                    <Link to="/nominate-page/nominate/">NOMINATE</Link>
+                    <Link to="/nominate-page/nominate/" id="header-link" activeStyle={activeLink}>NOMINATE</Link>
                 </li>
                 <li id="header-link">
-                    <Link to="/contact-us/contact-us/" activeStyle={{color:"red"}}>CONTACT US</Link>
+                    <Link to="/contact-us/contact-us/" id="header-link" activeStyle={activeLink}>CONTACT US</Link>
                 </li>
             </div>
         </div>
