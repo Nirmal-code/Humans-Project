f7e44e8872d197a7249185d563ccea356361f589
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-10-18 21:22:43 -0400

    added currently featured page

---
+++ humans-proj/src/pages/feature-page/currently-featured.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import "./currently-featured.css";
+
+export default function CurrFeaturePage() {
+    return (
+        <div id="curr-feature-page">
+            <Header />
+            <h1 style={{color:"black", align:"center"}}>This is the CURRENTLY FEATURED PAGE</h1>
+        </div>
+    )
+}
