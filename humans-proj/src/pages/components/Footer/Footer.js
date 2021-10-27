30233f95193b222f33287df6ba40c8c751a585e6
Zohair Ahmed <Zohair@Zohairs-MacBook-Pro.local>
2021-10-27 01:14:46 -0400

    Added links so that they share webpage

--- humans-proj/src/pages/components/Footer/Footer.js
+++ humans-proj/src/pages/components/Footer/Footer.js
@@ -1,31 +1,52 @@
 import React from "react";
 import './Footer.css';
 import { SocialIcon } from "react-social-icons";
+import {
+    FacebookShareButton, FacebookIcon,
+    TwitterShareButton, TwitterIcon,
+    WhatsappShareButton, WhatsappIcon
+} from "react-share";
 
 export default function Footer() {
+
+    const shareUrl = "https://www.softwareforlove.com/team"
+    const quote = "Take a look at some Humans."
+    const hashtag = "#softwareforlove #humans"
+
     return (
         <footer id="footer">
             <div id="wrapper">
-                <div id="footer-text" style={{color:"white", fontWeight:"lighter", float:"left", marginLeft: "3vw", marginRight:"1vw"}}>
-                    Let more people know about <span style={{fontWeight:"bolder"}}>Humans</span>
+                <div id="footer-text" style={{ color: "white", fontWeight: "lighter", float: "left", marginLeft: "3vw", marginRight: "1vw" }}>
+                    Let more people know about <span style={{ fontWeight: "bolder" }}>Humans</span>
                 </div>
                 <ul id="webSocialList">
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
-                    <li className="icon" id="share-icon" style={{visibility:"visible", marginRight:"1.5vw", marginTop:"1.0vh"}}>
+                    <li className="list-icon" id="share-icon" style={{ visibility: "visible", marginRight: "1.5vw", paddingTop: "0.5vh" }}>
                         <i className="fa fa-share-alt"></i>
                     </li>
-                    <li className="icon" id="insta-icon" style={{left:"-1.5vw"}}>
-                        <SocialIcon url="https://instagram.com" fgColor="black" bgColor="white" style={{ height: 60, width: 60 }}/>
+                    <li className="list-icon" id="insta-icon">
+                        <SocialIcon url="https://instagram.com"
+                            id="icon"
+                            bgColor="white"
+                            fgColor="black"
+                            target="_blank"
+                            style={{ width: "50px", height: "50px", paddingTop: "0.5vh" }} />
                     </li>
-                    <li className="icon" id="facebook-icon">
-                        <SocialIcon url="https://facebook.com" fgColor="black" bgColor="white" style={{ height: 60, width: 60 }}/>
+                    <li className="list-icon" id="facebook-icon">
+                        <FacebookShareButton id="button-icon" url={shareUrl} quote={quote} hashtag={hashtag} windowHeight={800} windowWidth={800}>
+                            <FacebookIcon id="icon" round={true} size={50} iconFillColor="black" bgStyle={{ fill: "white" }} />
+                        </FacebookShareButton>
                     </li>
-                    <li className="icon" id="twitter-icon">
-                        <SocialIcon url="https://twitter.com" fgColor="black" bgColor="white" style={{ height: 60, width: 60 }}/>
+                    <li className="list-icon" id="twitter-icon">
+                        <TwitterShareButton id="button-icon" url={shareUrl} title={quote} hashtag={hashtag} windowHeight={800} windowWidth={800}>
+                            <TwitterIcon id="icon" round={true} size={50} iconFillColor="black" bgStyle={{ fill: "white" }} />
+                        </TwitterShareButton>
                     </li>
-                    <li className="icon" id="whatsapp-icon">
-                        <SocialIcon url="https://whatsapp.com" fgColor="black" bgColor="white" style={{ height: 60, width: 60 }}/>
+                    <li className="list-icon" id="whatsapp-icon">
+                        <WhatsappShareButton id="button-icon" url={shareUrl} title={quote + "\n" + hashtag} separator=" " windowHeight={800} windowWidth={800}>
+                            <WhatsappIcon id="icon" round={true} size={50} iconFillColor="black" bgStyle={{ fill: "white" }} />
+                        </WhatsappShareButton>
                     </li>
                 </ul>
             </div>
