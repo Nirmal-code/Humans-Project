2458358a6fdbf50ce0b85e76b5d8fe968b2175d9
sings59 <sings59@mcmaster.ca>
2021-10-31 23:35:13 -0400

    65% finished about-us page

--- humans-proj/src/pages/about-page/AboutPage.js
+++ humans-proj/src/pages/about-page/AboutPage.js
@@ -2,12 +2,61 @@ import React from "react";
 import Header from "../components/Header/Header";
 import "./AboutPage.css";
 import Footer from "../components/Footer/Footer";
+import photo from '../../../static/images/about-us-claybanks.png';
+import photo2 from '../../../static/images/Rectangle.png';
 
 export default function AboutPage() {
     return (
         <div id="about-page">
             <Header />
-            <h1 style={{color:"black", align:"center"}}>This is the ABOUT PAGE</h1>
+            <section className= "image-section">
+                <button><h1>&lt;</h1></button>
+                <img className="changeable-image" src={photo} alt="" />
+                <img className="changeable-image" src={photo2} alt="" />
+                <button><h1>&gt;</h1></button>
+            </section>
+
+            <section className = "questions">
+                <div className= "first-row">
+                    <div className ="faq">
+                        <h2>What we do?</h2>
+                        <ul>
+                            <li>We help spread the word for good</li>
+                            <li>Created to spread awareness</li>
+                        </ul>
+                    </div>
+                    <div className ="faq">
+                        <h2>More About Software For Love</h2>
+                        <ul>
+                            <li>We help spread the word for good</li>
+                            <li>Created to spread awareness</li>
+                            <li>We help spread the word for good</li>
+                            <li>Created to spread awareness</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className= "second-row">
+                <div className ="faq">
+                    <h2>Why we do it?</h2>
+                    <ul>
+                        <li>We help spread the word for good</li>
+                        <li>Created to spread awareness</li>
+                        <li>We help spread the word for good</li>
+                        <li>Created to spread awareness</li>
+                    </ul>
+                </div>
+                <div className ="faq">
+                    <h2>Why we do it?</h2>
+                    <ul>
+                        <li>We help spread the word for good</li>
+                        <li>Created to spread awareness</li>
+                        <li>We help spread the word for good</li>
+                        <li>Created to spread awareness</li>
+                    </ul>
+                </div>
+                </div>
+
+            </section>
             <Footer />
         </div>
     )
