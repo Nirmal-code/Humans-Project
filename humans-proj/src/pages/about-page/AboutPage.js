40a5f578a16ccba20f0a6e1d42c795dcc18c0899
Fatimah Vakily <fvaki076@uottawa.ca>
2021-12-04 21:33:32 -0500

    fixed about page arrows, currently featured social media bg and made text layout plan for features easier to understand

--- humans-proj/src/pages/about-page/AboutPage.js
+++ humans-proj/src/pages/about-page/AboutPage.js
@@ -17,8 +17,8 @@ export default function AboutPage() {
                 <img className="changeable-image" src={photo} alt="" />
                 <img className="box" src={photo2} alt="" />
                 </div>
-                <button className = "arrow-button"><h1 className = "small-arrow">&gt;</h1></button>
                 <button className = "arrow-button"><h1 className = "big-arrow">&gt;</h1></button>
+                <button className = "arrow-button"><h1 className = "small-arrow">&gt;</h1></button>
             </section>
 
             <section className = "questions">
--- humans-proj/src/pages/feature-page/currently-featured.js
+++ humans-proj/src/pages/feature-page/currently-featured.js
@@ -16,13 +16,13 @@ export default function CurrFeaturePage() {
             <div class="square" id="square1"></div>
             <div class="square" id="square2"></div>
             <img src={photo} id="main_pic" className="photo-for-mainpage" alt="Photo"/>
-            <h1 id="main_text">Melissa Young</h1>
+            <h1 id="main_text">Name Here</h1>
             <p><button id="all_medias"><img src={insta} id="insta_pic" className="photo-for-mainpage" alt="Photo"/>&nbsp;<img src={facebook} id="facebook_pic" className="photo-for-mainpage" alt="Photo"/><img src={twitter} id="twitter_pic" className="photo-for-mainpage" alt="Photo"/><img src={internet} id="internet_pic" className="photo-for-mainpage" alt="Photo"/></button></p>
-            <p id='born'>Born: June 28, 1971 (age 50 years), Pretoria,</p>
-            <p id="worth">Net worth: 180.5 billion USD (2021) Forbes</p>
-            <p id="partner">Partner: Grimers(2018-)</p>
-            <p id="LG">LGBTQ+ activist</p>
-            <p id="para1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh nisl. Sit amet tellus cras adipiscing enim. Id porta nibh venenatis cras sed. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh nisl. Sit amet tellus cras adipiscing enim. Id porta nibh venenatis cras sed. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            <p id='born'>Key Fact 1 Here: Dolor sit amet</p>
+            <p id="worth">Key Fact 2 Here: Adipiscing elit</p>
+            <p id="partner">Key Fact 3 Here: Eiusmod tempor incididunt ut labore et</p>
+            <p id="LG">Type of Activist Here</p>
+            <p id="para1">Full Description Here ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh nisl. Sit amet tellus cras adipiscing enim. Id porta nibh venenatis cras sed. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh nisl. Sit amet tellus cras adipiscing enim. Id porta nibh venenatis cras sed. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             <p id="para2">Tortor id aliquet lectus proin nibh nisl. Sit amet tellus cras adipiscing enim. Id porta nibh venenatis cras sed. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor id aliquet lectus proin nibh nisl. Sit amet tellus cras adipiscing enim. Id porta nibh venenatis cras sed. Cursus euismod quis viverra nibh cras pulvinar mattis nunc. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Nulla aliquet enim tortor at.</p>
             <Footer />
         </div>
