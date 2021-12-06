1136e362d5ac49caaf960fc93ca6e82286c6e8e2
Yash Contractor <yash09@my.yorku.ca>
2021-12-06 14:44:04 -0500

    First commit, pushing this branch to remote, set up the cards layout

--- humans-proj/src/pages/feature-page/feature-page.js
+++ humans-proj/src/pages/feature-page/feature-page.js
@@ -2,12 +2,64 @@ import React from "react";
 import Header from "../components/Header/Header";
 import "./feature-page.css";
 import Footer from "../components/Footer/Footer";
+import photo from '../../../static/images/about-us-claybanks.png';
+import photo2 from '../../../static/images/Rectangle.png';
 
 export default function FeaturePage() {
     return (
         <div id="feature-page">
             <Header />
-            <h1 style={{color:"black", align:"center"}}>This is the ALL FEATURE PAGE</h1>
+            <div className="card-wrapper">
+                <div className="card">
+                    <h1>Name Lastname</h1>
+                    <div className="picture">
+                        <img
+                        src={photo2}
+                        alt="profile-picture"
+                        class="profile-img"
+                        ></img>
+                    </div>
+                    <div className="info">
+                        <h2>Lorem ispum</h2>
+
+                    </div>
+                </div>
+
+                <div className="card">
+                    <h1>Name Lastname</h1>
+                    <div className="picture">
+                        <img
+                        src={photo2}
+                        alt="profile-picture"
+                        class="profile-img"
+                        ></img>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card-wrapper">
+                <div className="card">
+                    <h1>Name Lastname</h1>
+                    <div className="picture">
+                        <img
+                        src={photo}
+                        alt="profile-picture"
+                        class="profile-img"
+                        ></img>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <h1>Name Lastname</h1>
+                    <div className="picture">
+                        <img
+                        src={photo}
+                        alt="profile-picture"
+                        class="profile-img"
+                        ></img>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div>
     )
