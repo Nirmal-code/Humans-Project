f931a986fec5a78cbc7fd0719c53130b8d8a163e
Yash Contractor <yash09@my.yorku.ca>
2021-12-15 19:41:36 -0500

    added the animation to the cards with the hover

--- humans-proj/src/pages/feature-page/feature-page.js
+++ humans-proj/src/pages/feature-page/feature-page.js
@@ -9,57 +9,25 @@ export default function FeaturePage() {
     return (
         <div id="feature-page">
             <Header />
-            <div className="card-wrapper">
-                <div className="card">
-                    <h1>Name Lastname</h1>
-                    <div className="picture">
-                        <img
-                        src={photo2}
-                        alt="profile-picture"
-                        class="profile-img"
-                        ></img>
-                    </div>
-                    <div className="info">
-                        <h2>Lorem ispum</h2>
-
+            <div id="curve" class="card">
+                <div class="card-blur">
+                    <img src={photo} class="img-responsive" alt="Cards Image"/>
+                    <div class="footer">
+                        <div class="info">
+                            <div class="name">Name Lastname</div>
+                            <div class="job">Postition Title</div>
+                            <div class="about">Lorem ipsum</div>
+                        </div>
+                        
                     </div>
                 </div>
-
-                <div className="card">
-                    <h1>Name Lastname</h1>
-                    <div className="picture">
-                        <img
-                        src={photo2}
-                        alt="profile-picture"
-                        class="profile-img"
-                        ></img>
-                    </div>
-                </div>
+                {/* <div class="connections">
+                    <div class="connection facebook"><div class="icon"></div></div>
+                    <div class="connection twitter"><div class="icon"></div></div>
+                    <div class="connection behance"><div class="icon"></div></div>
+                </div> */}
             </div>
-
-            <div className="card-wrapper">
-                <div className="card">
-                    <h1>Name Lastname</h1>
-                    <div className="picture">
-                        <img
-                        src={photo}
-                        alt="profile-picture"
-                        class="profile-img"
-                        ></img>
-                    </div>
-                </div>
-
-                <div className="card">
-                    <h1>Name Lastname</h1>
-                    <div className="picture">
-                        <img
-                        src={photo}
-                        alt="profile-picture"
-                        class="profile-img"
-                        ></img>
-                    </div>
-                </div>
-            </div>
+        
             <Footer />
         </div>
     )
