c482709908339f9ae7042cf445aac85df7bc2d40
Yash Contractor <yash09@my.yorku.ca>
2021-12-29 11:24:47 -0500

    Added social media icons, links still need to be added, and css needs to be changed

--- humans-proj/src/pages/feature-page/feature-page.js
+++ humans-proj/src/pages/feature-page/feature-page.js
@@ -9,6 +9,26 @@ export default function FeaturePage() {
     return (
         <div id="feature-page">
             <Header />
+
+        <div id="card-wrapper">
+            <div id="curve" class="card">
+                <div class="card-blur">
+                    <img src={photo} class="img-responsive" alt="Cards Image"/>
+                    <div class="footer">
+                        <div class="info">
+                            <div class="name">Name Lastname</div>
+                            <div class="job">Postition Title</div>
+                            <div class="about">Lorem ipsum</div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="connections">
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                </div>
+            </div>
+
             <div id="curve" class="card">
                 <div class="card-blur">
                     <img src={photo} class="img-responsive" alt="Cards Image"/>
@@ -27,7 +47,45 @@ export default function FeaturePage() {
                     <div class="connection behance"><div class="icon"></div></div>
                 </div> */}
             </div>
-        
+
+            <div id="curve" class="card">
+                <div class="card-blur">
+                    <img src={photo} class="img-responsive" alt="Cards Image"/>
+                    <div class="footer">
+                        <div class="info">
+                            <div class="name">Name Lastname</div>
+                            <div class="job">Postition Title</div>
+                            <div class="about">Lorem ipsum</div>
+                        </div>
+                        
+                    </div>
+                </div>
+                {/* <div class="connections">
+                    <div class="connection facebook"><div class="icon"></div></div>
+                    <div class="connection twitter"><div class="icon"></div></div>
+                    <div class="connection behance"><div class="icon"></div></div>
+                </div> */}
+            </div>
+
+            <div id="curve" class="card">
+                <div class="card-blur">
+                    <img src={photo} class="img-responsive" alt="Cards Image"/>
+                    <div class="footer">
+                        <div class="info">
+                            <div class="name">Name Lastname</div>
+                            <div class="job">Postition Title</div>
+                            <div class="about">Lorem ipsum</div>
+                        </div>
+                        
+                    </div>
+                </div>
+                {/* <div class="connections">
+                    <div class="connection facebook"><div class="icon"></div></div>
+                    <div class="connection twitter"><div class="icon"></div></div>
+                    <div class="connection behance"><div class="icon"></div></div>
+                </div> */}
+            </div>
+            </div>
             <Footer />
         </div>
     )
